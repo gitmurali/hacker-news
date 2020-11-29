c835a5e019adb592bb57b9dfc2b6e284f9f38411
Murali <murali.nutalapatii@gmail.com>
2020-11-29 10:47:49 +0530

    heroku app setup via circle ci config

--- config/webpack/base/webpack.base.shared.config.js
+++ config/webpack/base/webpack.base.shared.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'vSuite',
+      title: 'hacker-news',
       template: absolute('src/public/')
     })
   ]
--- src/styleguide/StyleGuideRenderer/StyleGuideRenderer.js
+++ src/styleguide/StyleGuideRenderer/StyleGuideRenderer.js
@@ -5,7 +5,7 @@ import styles from './StyleGuideRenderer.scss'
 const StyleGuideRenderer = ({ toc, children }) => (
   <main className={styles.main}>
     <div className={styles.sidebar}>
-      <h1 className={styles.title}>VSuite</h1>
+      <h1 className={styles.title}></h1>
       {toc}
     </div>
     <div className={styles.content}>
